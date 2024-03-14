0e737ebf1379f10a1d0756af625d91d005ba3922
Jakub Kuka <jakubkuka07@gmail.com>
2024-03-14 18:41:06 +0100

    last updates after testing

--- assignments/part1/my-first-react-app/src/App.js
+++ assignments/part1/my-first-react-app/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:8080/greeting')
+      .then(response => response.text())
+      .then(data => console.log(data))
+      .catch(error => console.error('There was an error!', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Your content here */}
+        <p>Check the console for the greeting!</p>
       </header>
     </div>
   );
