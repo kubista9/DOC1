3fd1fb9155e058e7bbe7f69ea46a8419e75ce05d
Jakub Kuka <jakubkuka07@gmail.com>
2024-03-14 18:20:23 +0100

    commit where everything is working, just commiting before testing

---
+++ assignments/part1/SpringBoot/src/main/java/com/example/myapp/config/WebConfig.java
@@ -0,0 +1,5 @@
+package com.example.myapp.config;
+
+public class WebConfig {
+
+}
--- assignments/part1/SpringBoot/src/main/java/com/example/myapp/controllers/GreetingController.java
+++ assignments/part1/SpringBoot/src/main/java/com/example/myapp/controllers/GreetingController.java
@@ -1,4 +1,4 @@
-package controllers;
+package com.example.myapp.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
