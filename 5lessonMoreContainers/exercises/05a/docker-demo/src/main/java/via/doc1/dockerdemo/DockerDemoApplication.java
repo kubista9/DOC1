907c825d1cce358be341c657fbd4330509bd10a1
Jakub Kuka <jakubkuka07@gmail.com>
2024-05-06 11:27:51 +0200

    Exercise 05a docker demo init

--- 5lessonMoreContainers/exercises/05a/docker-demo/src/main/java/via/doc1/dockerdemo/DockerDemoApplication.java
+++ 5lessonMoreContainers/exercises/05a/docker-demo/src/main/java/via/doc1/dockerdemo/DockerDemoApplication.java
@@ -2,17 +2,21 @@ package via.doc1.dockerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerDemoApplication {
-
-	public String index() {return "<body><h1>DevOps and Cloud</h1> " +
-		"<h2>Dockerizing Spring Boot Backend Application.</h2>" +
-		"<p>With Docker, we can containerize SEP4 back-end and front-end applications.</p></body>";
+	@RequestMapping("/")
+	public String index() {
+		return "<body><h1> Containerization and Orchestration</h1>" +
+			"<h2> Dockerize and Deploy Spring Boot Backend Application.</h2>" +
+		"<p>With docker, we can containerize SEP4 backend and frontend applications.</p>" +
+		"<p›We can also deploy the applications to <b>Kubernetes Cluster</b>.</p>" +
+			"</body>";
 	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoApplication.class, args);
 	}
-
 }
